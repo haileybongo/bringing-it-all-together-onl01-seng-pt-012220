90faf4eead3448959229548e6f01fececbfdb878
Hailey Bongo <haileyebongo@gmail.com>
2020-04-02 22:02:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,6 +67,11 @@ class Dog
     new_student.save
   end
   
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
   
   
 end
