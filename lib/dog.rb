53c50de46cea2967247c518937fcc5950ef37209
Hailey Bongo <haileyebongo@gmail.com>
2020-04-02 21:57:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,6 +26,21 @@ class Dog
     DB[:conn].execute(sql)
   end
   
+    def save
+  if self.id
+    self.update
+  else
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  end
+  
   def self.new_from_db(array)
     id = array[0]
     name = array[1]
