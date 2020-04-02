ae4e5436ae2798775c55573de692fe7dd66519b7
Hailey Bongo <haileyebongo@gmail.com>
2020-04-02 22:07:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
   end
   
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    sql = "UPDATE dog SET name = ?, be = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
